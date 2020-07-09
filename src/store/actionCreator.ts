823bf9adbb02c5f85bd3314d16ff683b65463be8
denis.dev <denis.dev@kakaoenterprise.com>
2020-07-09 23:52:42 +0900

    chore: add files for redux
    Ducks 패턴 적용할 예정

---
+++ src/store/actionCreator.ts
@@ -0,0 +1,4 @@
+import { bindActionCreators } from 'redux';
+import store from './index';
+
+const { dispatch } = store;
---
+++ src/store/configure.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import modules from './modules';
+
+const configure = () => {
+  const store = createStore(modules);
+  // const devTools =
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  //   window.__REDUX_DEVTOOLS_EXTENSION__();
+  // const store = createStore(modules, devTools);
+  return store;
+};
+
+export default configure;
