9ea36d1d3632511b4ae67571f9b8c6c53218e032
SeWon Kim <swon962@gmail.com>
2020-07-23 23:25:22 +0900

    style: 💄 add question page

---
+++ src/pages/question/AnalysisPage.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+import * as S from './styled';
+
+type AnalysisPageProps = {};
+
+export default function MainPage(_: AnalysisPageProps): ReactElement {
+  return <S.Container>MainPage</S.Container>;
+}
