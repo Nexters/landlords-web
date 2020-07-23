2b1ce6a58c4538ad40eb41a584d3abdbd7257397
SeWon Kim <swon962@gmail.com>
2020-07-23 23:26:13 +0900

    feat: 🎸 button onClick 이벤트 추가

--- src/pages/persona/PersonaAnalysis.tsx
+++ src/pages/persona/PersonaAnalysis.tsx
@@ -1,8 +1,11 @@
+import { History } from 'history';
 import React, { ReactElement } from 'react';
 
 import * as S from './styled';
 
-type PersonaAnalysisPageProps = {};
+type PersonaAnalysisPageProps = {
+  history: History;
+};
 
 enum TEXT {
   title = '당신만을 위한 자취방 체크리스트를 만들어드릴게요!',
@@ -11,15 +14,22 @@ enum TEXT {
   back = '아니요, 괜찮아요',
 }
 
-export default function PersonaAnalysisPage(_: PersonaAnalysisPageProps): ReactElement {
+export default function PersonaAnalysisPage({ history }: PersonaAnalysisPageProps): ReactElement {
+  const goQuestion = () => {
+    history.push('/persona/question');
+  };
+  const goBack = () => {
+    history.goBack();
+  };
+
   return (
     <S.Container>
       <S.Title>{TEXT.title}</S.Title>
       <S.Description>{TEXT.description}</S.Description>
 
       <S.CounterDescription>총 명이 체크해방을 참고했습니다.</S.CounterDescription>
-      <S.StartButton>{TEXT.start}</S.StartButton>
-      <S.BackButton>{TEXT.back}</S.BackButton>
+      <S.StartButton onClick={goQuestion}>{TEXT.start}</S.StartButton>
+      <S.BackButton onClick={goBack}>{TEXT.back}</S.BackButton>
     </S.Container>
   );
 }
