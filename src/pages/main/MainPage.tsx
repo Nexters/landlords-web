f799d2ecf31daaa6482e979b5589ab40c09f65d4
Kong Chae Won <chaewonkong@gmail.com>
2020-07-24 23:59:19 +0900

    refactor: 💡 remove modal from main

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,26 +1,13 @@
 import React, { ReactElement, useState } from 'react';
 
-import Modal from '../../components/modal';
 import * as S from './styled';
 
 type MainPageProps = {};
 
 export default function MainPage(): ReactElement {
-  const [modalVisible, setModalVisible] = useState(false);
-
   return (
     <S.Container>
-      <Modal 
-        className='modal-root'
-        closable={ true }
-        onClose={ () => setModalVisible(!modalVisible) }
-        maskClosable={ true }
-        visible={ modalVisible }
-      >
-        <div>Modal</div>
-      </Modal>
-      <button onClick={ () => setModalVisible(true) }>모달 열기 </button>
+      <div>hi</div>
     </S.Container>
-
   );
 }
