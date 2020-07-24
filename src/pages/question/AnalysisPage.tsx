3a3b4526295030e867130f53e80157c29eb699cd
SeWon Kim <swon962@gmail.com>
2020-07-24 12:54:46 +0900

    chore: component명 변경

--- src/pages/question/AnalysisPage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-
-import * as S from './styled';
-
-type AnalysisPageProps = {};
-
-export default function MainPage(_: AnalysisPageProps): ReactElement {
-  return <S.Container>MainPage</S.Container>;
-}
---
+++ src/pages/question/PersonaQuestion.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+import * as S from './styled';
+
+type PersonaQuestionPageProps = {};
+
+export default function PersonaQuestionPage(_: PersonaQuestionPageProps): ReactElement {
+  return <S.Container>PersonaQuestionPage</S.Container>;
+}
