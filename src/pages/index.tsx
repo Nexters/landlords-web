df256dda6dea98617428953f37fedcd1939b87d3
Kong Chae Won <chaewonkong@gmail.com>
2020-07-25 05:17:38 +0900

    refactor: 💡 change redux structure to follow ducks pattern

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import {createStore} from '../store';
 import ChecklistPage from './checklist/ChecklistPage';
 import MainPage from './main/MainPage';
 import PersonaAnalysisPage from './persona/PersonaAnalysis';
-import RoomComparisonPage from './room/RoomComparison';
+import RoomComparisonPage from './room/RoomComparison';;
 
 export default function EntryRoute() {
+  const store = createStore();
+
   return (
-    <BrowserRouter>
-      <Route path='/' exact component={MainPage} />
-      <Route path='/checklist' component={ChecklistPage} />
-      <Route path='/persona' component={PersonaAnalysisPage} />
-      <Route path='/room' component={RoomComparisonPage} />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route path='/' exact component={MainPage} />
+        <Route path='/checklist' component={ChecklistPage} />
+        <Route path='/persona' component={PersonaAnalysisPage} />
+        <Route path='/room' component={RoomComparisonPage} />
+      </BrowserRouter>
+    </Provider>
   );
 }
