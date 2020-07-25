ea3cf8a350003c6f3c8940d17c7300a6fc40e5a1
SeWon Kim <swon962@gmail.com>
2020-07-26 01:14:33 +0900

    refactor: 💡 change directory of card component

--- src/pages/question/PersonaQuestion.tsx
+++ src/pages/question/PersonaQuestion.tsx
@@ -1,7 +1,7 @@
-import Card from 'components/card';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import Card from './card';
 import questions from './questions';
 import * as S from './styled';
 
