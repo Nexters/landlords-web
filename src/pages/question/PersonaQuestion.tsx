a405d45551b7d2f2eb3a40ee4952404403dba42b
SeWon Kim <swon962@gmail.com>
2020-07-25 23:22:25 +0900

    feat: 🎸 question 임시 데이터 추가 & question id 출력

--- src/pages/question/PersonaQuestion.tsx
+++ src/pages/question/PersonaQuestion.tsx
@@ -1,16 +1,39 @@
-import React, { ReactElement } from 'react';
+import Card from 'components/card';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
+import questions from './questions';
 import * as S from './styled';
 
 export default function PersonaQuestionPage(): ReactElement {
+  const history = useHistory();
+
+  const [questionNum, setQuestionNum] = useState(1);
+  const [quesitonLen] = useState(questions.length);
+
+  useEffect(() => {
+    questionNum >= quesitonLen ? history.push('/persona/result') : '';
+  }, [questionNum]);
+
+  const handleCardClick = () => {
+    setQuestionNum(questionNum + 1);
+  };
+
   return (
     <S.Container>
       <S.BackButton>-</S.BackButton>
 
-      <S.QuestionID>question_id</S.QuestionID>
+      <S.QuestionID>
+        {questionNum}/{quesitonLen}
+      </S.QuestionID>
       <S.Title>title</S.Title>
 
-      <S.CardDiv>card</S.CardDiv>
+      <S.CardDiv>
+        <Card uid={1} contents='남자' onClick={handleCardClick}></Card>
+        <Card uid={1} contents='남자' onClick={handleCardClick}></Card>
+        <Card uid={1} contents='남자' onClick={handleCardClick}></Card>
+        <Card uid={1} contents='남자' onClick={handleCardClick}></Card>
+      </S.CardDiv>
 
       <S.ProgressDiv></S.ProgressDiv>
     </S.Container>
