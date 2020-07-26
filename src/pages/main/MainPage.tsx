2eef4aa3cc00eb4bb1f0da63009663aa71a33eb5
Kong Chae Won <chaewonkong@gmail.com>
2020-07-26 15:39:41 +0900

    feat: 🎸 connect redux to PersonaQuestion

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,27 +1,8 @@
 import React, { ReactElement, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
-import { personaQuestionSelector,questionsAction } from '../../store/personaQuestionsSlice';
 import * as S from './styled';
 
-type MainPageProps = {};
-
-export default function MainPage(_: MainPageProps): ReactElement {
-  const dispatch = useDispatch();
-  const questions = useSelector(personaQuestionSelector);
-
-  useEffect(() => {
-    dispatch(questionsAction.setQuestions([
-      {
-        uid: 2, contents: 'abcd',
-        choice_items: [{ uid: 12, contents: 'abcc' }],
-        is_multi_choice: true,
-      },
-    ],
-    ));
-  }, []);
-
-  console.log('questions', questions);
+export default function MainPage(): ReactElement {
 
   return <S.Container>MainPage</S.Container>;
 }
--- src/pages/question/PersonaQuestion.tsx
+++ src/pages/question/PersonaQuestion.tsx
@@ -1,9 +1,26 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { personaQuestionSelector,questionsAction } from '../../store/personaQuestionsSlice';
 import * as S from './styled';
 
 type PersonaQuestionPageProps = {};
 
 export default function PersonaQuestionPage(_: PersonaQuestionPageProps): ReactElement {
+  const dispatch = useDispatch();
+  const questions = useSelector(personaQuestionSelector);
+
+  useEffect(() => {
+    dispatch(questionsAction.setQuestions([
+      {
+        uid: 2, contents: 'abcd',
+        choice_items: [{ uid: 12, contents: 'abcc' }],
+        is_multi_choice: true,
+      },
+    ],
+    ));
+  }, []);
+
+  console.log('questions', questions);
   return <S.Container>PersonaQuestionPage</S.Container>;
 }
