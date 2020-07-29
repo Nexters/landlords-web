e65006715a0dcbe6ac413f911f5f3c3eb9e3e57c
denis.dev <denis.dev@kakaoenterprise.com>
2020-07-30 03:30:35 +0900

    modify: Checkbox svg 스위칭 방식으로 변경

--- src/components/checkbox/styled.ts
+++ src/components/checkbox/styled.ts
@@ -2,15 +2,10 @@ import styled from 'styled-components';
 
 const Container = styled.span`
   display: inline-block;
-  position: relative;
-  width: 18px;
-  height: 18px;
-  vertical-align: -4px;
 `;
 
 const StyledInput = styled.input`
-  width: 24px;
-  height: 24px;
+  margin: 0;
 `;
 
 export { Container, StyledInput };
