de5d9ff7fe7cbc3b4949d0c3dabfba4fd7b47ba5
SeWon Kim <swon962@gmail.com>
2020-07-30 01:48:21 +0900

    style: 💄 persona Analysis, Result Page style 수정

--- src/pages/persona/PersonaAnalysis.tsx
+++ src/pages/persona/PersonaAnalysis.tsx
@@ -7,7 +7,7 @@ enum TEXT {
   TITLE = '당신만을 위한\n자취방 체크리스트를\n만들어드릴게요!',
   DESCRIPTION = '방 구하기부터 이사까지,\n그 누구도 알려주지 않은 맞춤형 꿀팁',
   START = '자취 준비 시작',
-  BACK = '아니요, 괜찮아요',
+  BACK = '아니요, 다음에 할게요!',
 }
 
 export default function PersonaAnalysisPage(): ReactElement {
@@ -20,8 +20,8 @@ export default function PersonaAnalysisPage(): ReactElement {
 
   const sortedTitle = TEXT.TITLE.split('\n').map((line) => {
     return (
-      <span key={ line }>
-        { line }
+      <span key={line}>
+        {line}
         <br />
       </span>
     );
@@ -29,8 +29,8 @@ export default function PersonaAnalysisPage(): ReactElement {
 
   const sortedDesc = TEXT.DESCRIPTION.split('\n').map((line) => {
     return (
-      <span key={ line }>
-        { line }
+      <span key={line}>
+        {line}
         <br />
       </span>
     );
@@ -38,15 +38,17 @@ export default function PersonaAnalysisPage(): ReactElement {
 
   return (
     <S.Container>
-      <S.Title>{ sortedTitle }</S.Title>
-      <S.Description> { sortedDesc }</S.Description>
+      <S.Title>{sortedTitle}</S.Title>
+      <S.Description> {sortedDesc}</S.Description>
 
-      <S.CounterDescription>총 { viewerCount }명이 체크해방을 참고했습니다.</S.CounterDescription>
+      <S.CounterDescription>
+        총 <S.Count>{viewerCount}</S.Count>명이 체크해방을 참고했습니다.
+      </S.CounterDescription>
 
       <Link to='/persona/question'>
-        <S.StartButton>{ TEXT.START }</S.StartButton>
+        <S.StartButton>{TEXT.START}</S.StartButton>
       </Link>
-      <S.BackButton onClick={ handleBackButton }>{ TEXT.BACK }</S.BackButton>
+      <S.BackButton onClick={handleBackButton}>{TEXT.BACK}</S.BackButton>
     </S.Container>
   );
 }
