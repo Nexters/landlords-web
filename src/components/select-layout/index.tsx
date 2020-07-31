8e663ee73d53551e1f514a45f3846319b355bed7
denis.dev <denis.dev@kakaoenterprise.com>
2020-07-31 14:42:22 +0900

    feat: SelectLayout 컴포넌트 구현
    선택형 체크리스트를 라벨별로 출력

--- src/components/select-layout/index.tsx
+++ src/components/select-layout/index.tsx
@@ -1,41 +1,53 @@
 import { Checkbox, Icon } from 'components';
-import { SelectItem } from 'entity/checklist';
-import React, { ChangeEvent, MouseEvent, ReactElement } from 'react';
+import { SelectItem, SimpleCheckItem } from 'entity/checklist';
+import React, { ReactElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { roomsAction, roomsSelector } from 'store/roomsSlice';
 
 import * as S from './styled';
 
-interface SelectLayoutProps {
-  label: string;
-  items: SelectItem[];
-  onClick?: (index: number) => void;
-}
+export default function SelectLayout(): ReactElement {
+  const { selectItemsByLabel } = useSelector(roomsSelector);
+  const { setSelectItemsByLabel } = roomsAction;
+  const dispatch = useDispatch();
 
-export default function SelectLayout({
-  label,
-  items,
-  onClick = () => {},
-}: SelectLayoutProps): ReactElement {
+  const handleCheckboxClick = (label: string, selectIndex: number, checkIndex: number) => () => {
+    const selectItems = selectItemsByLabel[label].slice();
+    const checkItems = (selectItems[selectIndex].value as SimpleCheckItem[]).slice();
+    checkItems[checkIndex] = {
+      ...checkItems[checkIndex],
+      checked: !checkItems[checkIndex].checked,
+    };
+    selectItems[selectIndex] = { ...selectItems[selectIndex], value: checkItems };
+    const nextSelectItemsByLabel = { ...selectItemsByLabel, [label]: selectItems };
+    dispatch(setSelectItemsByLabel(nextSelectItemsByLabel));
+  };
+
   return (
-    <S.Container>
-      <S.TitleLayer>
-        <span>{label}</span>
-        <Icon name='NAVIGATION_OPEN_DOWN' size='22' />
-      </S.TitleLayer>
-      {items.map((item, index) => (
-        <S.CheckItemWrapper key={index}>
-          <span>{item.description}</span>
-          <S.CheckboxGroup>
-            {item.value.map((checkItem) => (
-              <Checkbox
-                key={checkItem.name}
-                label={checkItem.name}
-                checked={checkItem.selected}
-                onClick={() => onClick(index)}
-              />
-            ))}
-          </S.CheckboxGroup>
-        </S.CheckItemWrapper>
+    <>
+      {Object.keys(selectItemsByLabel).map((label) => (
+        <S.Container key={label}>
+          <S.TitleLayer>
+            <span>{label}</span>
+            <Icon name='NAVIGATION_OPEN_DOWN' size='22' />
+          </S.TitleLayer>
+          {selectItemsByLabel[label].map((selectItem, selectIndex) => (
+            <S.CheckItemWrapper key={selectIndex}>
+              <span>{selectItem.description}</span>
+              <S.CheckboxGroup>
+                {(selectItem as SelectItem).value.map((item, checkIndex) => (
+                  <Checkbox
+                    key={item.name}
+                    label={item.name}
+                    checked={item.checked}
+                    onClick={handleCheckboxClick(label, selectIndex, checkIndex)}
+                  />
+                ))}
+              </S.CheckboxGroup>
+            </S.CheckItemWrapper>
+          ))}
+        </S.Container>
       ))}
-    </S.Container>
+    </>
   );
 }
