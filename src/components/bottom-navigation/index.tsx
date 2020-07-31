27cf50058ff1f1e7f113eee6eb11769a4c30aa81
Kong Chae Won <chaewonkong@gmail.com>
2020-08-01 04:34:58 +0900

    refactor: 💡 use text to store state

--- src/components/bottom-navigation/index.tsx
+++ src/components/bottom-navigation/index.tsx
@@ -8,18 +8,14 @@ export default function BottomNavigation() {
     WIKI = '백과사전',
     EXPLORE = '둘러보기'
   }
-  const generateKey = (source: string) => `key-${source}`;
-  const [selected, setSelected] = useState(generateKey(TEXT.CHECKLIST));
-
-  const navigationItems = [TEXT.CHECKLIST, TEXT.WIKI, TEXT.EXPLORE].map((text) => {
-    const key = generateKey(text);
-    return(
-      <S.NavigationItem key={key} onClick={() => setSelected(key)}>
-        <S.IconWrapper selected={key === selected} />
-        <S.Text>{text}</S.Text>
-      </S.NavigationItem>
-    );
-  });
+  const [selected, setSelected] = useState(TEXT.CHECKLIST);
+  const navigationItems = [TEXT.CHECKLIST, TEXT.WIKI, TEXT.EXPLORE].map((text) => (
+    <S.NavigationItem key={`key-${text}`} onClick={() => setSelected(text)}>
+      <S.IconWrapper selected={text === selected} />
+      <S.Text>{text}</S.Text>
+    </S.NavigationItem>
+  ),
+  );
 
   return (
     <S.Container>
