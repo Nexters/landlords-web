adb1608a4aa29b67a29ac2ec84926da46010a74b
denis.dev <denis.dev@kakaoenterprise.com>
2020-08-14 17:27:49 +0900

    modify: add props in Room Interface
    RoomsResponse Interface 추가

--- src/entity/response.ts
+++ src/entity/response.ts
@@ -1,5 +1,11 @@
+import { Room } from './rooms';
+
 export interface Response<T = object> {
   data: T;
   error: boolean;
   message?: string;
+}
+
+export interface RoomsResponse {
+  rooms: Room[];
 }
--- src/entity/rooms.ts
+++ src/entity/rooms.ts
@@ -8,5 +8,11 @@ export interface Room {
   description: string;
   image: string;
   building_type: number;
+  room_size: number;
+  floor: string;
+  has_elevator: boolean;
+  administrative_expenses: number;
   user_id: number;
+  name?: string;
+  price?: string;
 }
