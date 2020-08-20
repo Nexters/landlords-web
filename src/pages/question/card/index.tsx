aa8a0dac844dbd3024d6d89b765081317e14a87a
SeWon Kim <swon962@gmail.com>
2020-08-21 00:00:28 +0900

    style: 💄 card contents에 highlight 추가

--- src/pages/question/card/index.tsx
+++ src/pages/question/card/index.tsx
@@ -10,9 +10,27 @@ interface CardProps {
 }
 
 export default function Card({ uid, contents, index, onClick }: CardProps) {
+  const sortedContents = contents.split(/\\n/).map((line) => {
+    const highlight = line.includes('<hr>')
+      ? line.substring(line.lastIndexOf('<hr>') + 4, line.lastIndexOf('</hr>'))
+      : '';
+    const prevHighlight = line.split('<hr>')[0];
+    const nextHighlight = line.split('</hr>')[1];
+    return (
+      <>
+        <span key={line}>
+          {prevHighlight}
+          <S.Highlight>{highlight}</S.Highlight>
+          {nextHighlight}
+        </span>
+        <br />
+      </>
+    );
+  });
+
   return (
     <S.CardWrapper onClick={() => onClick(index)} data-index={uid}>
-      <S.CardContent>{contents}</S.CardContent>
+      <S.CardContent>{sortedContents}</S.CardContent>
     </S.CardWrapper>
   );
 }
